b6ba289ae91e4924bfd05b3c6c745d5be97b219f
aswini-thiru <aswinithirumaran@gmail.com>
2024-03-08 19:09:17 +0100

    Working on the App interfaces

---
+++ part-1/SDR_App/SDR_App/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace SDR_App;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnGoToNextPageButtonClick(object sender, EventArgs e)
+    {
+        // Navigate to the next page
+        Navigation.PushAsync(new Page1());
+    }
+
+    private async void OnAttachFileButtonClick(object sender, EventArgs e)
+    {
+        // Prompt the user to pick a CSV file
+        var result = await FilePicker.PickAsync(options: new PickOptions { FileTypes = FilePickerFileType.Csv, PickerTitle = "Pick a CSV File" });
+
+        if (result != null)
+        {
+            // Handle the selected CSV file (result.FullPath)
+            // You can perform further processing or store the file path as needed
+            DisplayAlert("File Selected", $"File selected: {result.FileName}", "OK");
+        }
+    }
+
+}
+
---
+++ part-1/SDR_App/SDR_App/Page1.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SDR_App;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
