14651f4a9cfa51efb0228601e59be49d3299fd3a
aswini-thiru <aswinithirumaran@gmail.com>
2024-03-10 15:36:30 +0100

    Application setup

--- part-1/SDR_App/SDR_App/MainPage.xaml.cs
+++ part-1/SDR_App/SDR_App/MainPage.xaml.cs
@@ -14,18 +14,4 @@ public partial class MainPage : ContentPage
         Navigation.PushAsync(new Page1());
     }
 
-    private async void OnAttachFileButtonClick(object sender, EventArgs e)
-    {
-        // Prompt the user to pick a CSV file
-        var result = await FilePicker.PickAsync(options: new PickOptions { FileTypes = FilePickerFileType.Csv, PickerTitle = "Pick a CSV File" });
-
-        if (result != null)
-        {
-            // Handle the selected CSV file (result.FullPath)
-            // You can perform further processing or store the file path as needed
-            DisplayAlert("File Selected", $"File selected: {result.FileName}", "OK");
-        }
-    }
-
 }
-
